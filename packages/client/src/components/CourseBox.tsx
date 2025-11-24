e4f942c81f3e58d1db4015fd2640200bb45820f2
eserven <emily.e.serven@gmail.com>
2025-11-23 21:10:59 -0500

    Added realer test data

---
+++ packages/client/src/components/CourseBox.tsx
@@ -0,0 +1,100 @@
+import type { Course } from "@/routes/courses";
+
+import { CheckCheck, CheckCircle, DollarSign, ExternalLink, PauseCircle, PlayCircle, Timer } from "lucide-react";
+
+export function CourseBox({
+  key,
+  status,
+  link,
+  name,
+  topic,
+  dateExpires,
+  progressCurrent,
+  progressTotal,
+  cost,
+}: Course) {
+  return (
+    <div
+      key={key}
+      className="flex flex-col gap-2 rounded border p-2"
+    >
+      <div className="flex items-start justify-between">
+        <div className="flex flex-row items-start gap-2">
+          <div className="mt-2">
+            {status && status === "inactive" && (
+              <PauseCircle size={20} />
+            )}
+            {status && status === "active" && (
+              <PlayCircle size={20} />
+            )}
+            {status && status === "complete" && (
+              <CheckCircle size={20} />
+            )}
+          </div>
+          <h3 className="text-2xl">{name}</h3>
+        </div>
+        <a
+          href={link}
+          target="_blank"
+          className="mt-1.5 cursor-pointer"
+          rel="noopener noreferrer"
+        >
+          <ExternalLink size={16} />
+        </a>
+      </div>
+      <p>{topic}</p>
+      <div className="flex flex-row justify-between gap-8">
+        <div className="flex flex-row items-center gap-1">
+          <Timer size={16} />
+          {dateExpires
+            ? dateExpires
+            : (
+              <i
+                className="text-sm"
+              >No course expiry given
+              </i>
+            )}
+        </div>
+        <div className="flex flex-row items-center gap-2">
+          <CheckCheck size={16} />
+          {progressCurrent && progressTotal
+            ? `${progressCurrent} / ${progressTotal}`
+            : (
+              <i
+                className="text-sm"
+              >No progress
+              </i>
+            )}
+        </div>
+        <div className="flex flex-row items-center">
+          <DollarSign size={16} />
+          {cost
+            ? `${cost}`
+            : (
+              <i
+                className="text-sm"
+              >No cost given
+              </i>
+            )}
+        </div>
+      </div>
+      {
+        progressCurrent && progressTotal && (
+          <div className="bg-secondary mt-2 w-full">
+            <div
+              className={`
+                ${status && status === "inactive"
+            ? "bg-primary/50"
+            : "bg-primary"}
+                h-1 rounded
+              `}
+              style={{
+                width: `${progressTotal / progressCurrent}%`,
+              }}
+            />
+          </div>
+        )
+      }
+    </div>
+  );
+}
--- packages/client/src/routes/courses.tsx
+++ packages/client/src/routes/courses.tsx
@@ -1,19 +1,12 @@
 import { createFileRoute } from "@tanstack/react-router";
-import {
-  CheckCheck,
-  CheckCircle,
-  Circle,
-  DollarSign,
-  ExternalLink,
-  PauseCircle,
-  Timer,
-} from "lucide-react";
+
+import { CourseBox } from "@/components/CourseBox";
 
 export const Route = createFileRoute("/courses")({
   component: Courses,
 });
 
-interface Course {
+export interface Course {
   name: string;
   key: string;
   link: string;
@@ -40,86 +33,10 @@ export function Courses() {
               return;
             }
             return (
-              <div
+              <CourseBox
+                {...course}
                 key={course.key}
-                className="flex flex-col gap-2 rounded border p-2"
-              >
-                <div className="flex items-start justify-between">
-                  <div className="flex flex-row items-center gap-2">
-                    {course.status && course.status === "inactive" && (
-                      <PauseCircle size={20} />
-                    )}
-                    {course.status && course.status === "active" && (
-                      <Circle size={20} />
-                    )}
-                    {course.status && course.status === "complete" && (
-                      <CheckCircle size={20} />
-                    )}
-                    <h3 className="text-2xl">{course.name}</h3>
-                  </div>
-                  <a
-                    href={course.link}
-                    target="_blank"
-                    className="cursor-pointer"
-                    rel="noopener noreferrer"
-                  >
-                    <ExternalLink size={16} />
-                  </a>
-                </div>
-                <p>{course.topic}</p>
-                <div className="flex flex-row gap-8">
-                  <div className="flex flex-row items-center gap-1">
-                    <Timer size={16} />
-                    {course?.dateExpires
-                      ? course.dateExpires
-                      : (
-                        <i
-                          className="text-sm"
-                        >No course expiry given
-                        </i>
-                      )}
-                  </div>
-                  <div className="flex flex-row items-center gap-2">
-                    <CheckCheck size={16} />
-                    {course?.progressCurrent && course?.progressTotal
-                      ? `${course.progressCurrent} / ${course.progressTotal}`
-                      : (
-                        <i
-                          className="text-sm"
-                        >No progress
-                        </i>
-                      )}
-                  </div>
-                  <div className="flex flex-row items-center">
-                    <DollarSign size={16} />
-                    {course?.cost
-                      ? `${course.cost}`
-                      : (
-                        <i
-                          className="text-sm"
-                        >No cost given
-                        </i>
-                      )}
-                  </div>
-                </div>
-                {
-                  course.progressCurrent && course.progressTotal && (
-                    <div className="bg-secondary mt-2 w-full">
-                      <div
-                        className={`
-                          ${course?.status && course.status === "inactive"
-                      ? "bg-primary/50"
-                      : "bg-primary"}
-                          h-1 rounded
-                        `}
-                        style={{
-                          width: `${course.progressTotal / course.progressCurrent}%`,
-                        }}
-                      />
-                    </div>
-                  )
-                }
-              </div>
+              />
             );
           })
         }
