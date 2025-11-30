223f7b5dcd11738387fcff8bbca6694788b5d452
eserven <emily.e.serven@gmail.com>
2025-11-29 23:49:43 -0500

    Added courseProviders and courses tables

--- packages/middleware/src/db/schema.ts
+++ packages/middleware/src/db/schema.ts
@@ -1,4 +1,5 @@
-import { boolean, integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { boolean, integer, numeric, pgEnum, pgTable, varchar } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -11,3 +12,46 @@ export const usersTable = pgTable("users", {
     length: 255,
   }).notNull().unique(),
 });
+
+export const recurPeriodUnitEnum = pgEnum("recurPeriodUnit", ["days", "months", "years"]);
+
+export const courseProviders = pgTable("courseProviders", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({
+    length: 255,
+  }).notNull(),
+  url: varchar({
+    length: 255,
+  }).notNull(),
+  cost: numeric(),
+  isRecurring: boolean().notNull(),
+  recurPeriodUnit: recurPeriodUnitEnum(),
+  recurPeriod: integer().notNull(),
+  isCourseFeesShared: boolean().notNull(),
+});
+
+export const courses = pgTable("courses", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({}).notNull(),
+  url: varchar({
+    length: 255,
+  }).notNull(),
+  isCostFromPlatform: boolean().notNull(),
+  cost: numeric().notNull(),
+  courseProviderId: integer("course_provider_id"),
+});
+
+export const courseProviderRelations = relations(courseProviders, ({
+  many,
+}) => ({
+  courses: many(courses),
+}));
+
+export const coursesRelations = relations(courses, ({
+  one,
+}) => ({
+  courseProvider: one(courseProviders, {
+    fields: [courses.courseProviderId],
+    references: [courseProviders.id],
+  }),
+}));
