b1b414d755cc76ad73cd236a6173ffdc657d398d
eserven <emily.e.serven@gmail.com>
2025-12-07 20:24:09 -0500

    Added tooltips to activity status

--- packages/client/src/components/StatusIndicator.tsx
+++ packages/client/src/components/StatusIndicator.tsx
@@ -2,19 +2,44 @@ import type { CourseStatus } from "@emstack/types/src";
 
 import { CheckCircle, PauseCircle, PlayCircle } from "lucide-react";
 
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/tooltip";
+
+interface StatusIndicatorProps { status: CourseStatus }
+
 export function StatusIndicator({
   status,
-}: { status: CourseStatus }) {
+}: StatusIndicatorProps) {
   return (
-    <div>
+    <div className="flex items-center justify-center">
       {status && status === "inactive" && (
-        <PauseCircle size={16} />
+        <Tooltip>
+          <TooltipTrigger>
+            <PauseCircle size={16} />
+          </TooltipTrigger>
+          <TooltipContent>
+            Paused
+          </TooltipContent>
+        </Tooltip>
       )}
       {status && status === "active" && (
-        <PlayCircle size={16} />
+        <Tooltip>
+          <TooltipTrigger>
+            <PlayCircle size={16} />
+          </TooltipTrigger>
+          <TooltipContent>
+            Active
+          </TooltipContent>
+        </Tooltip>
       )}
       {status && status === "complete" && (
-        <CheckCircle size={16} />
+        <Tooltip>
+          <TooltipTrigger>
+            <CheckCircle size={16} />
+          </TooltipTrigger>
+          <TooltipContent>
+            Completed
+          </TooltipContent>
+        </Tooltip>
       )}
     </div>
   );
