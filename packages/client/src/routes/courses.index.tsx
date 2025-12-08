f466c5414c3d17e6161d683e1b9948ac410bf5ba
eserven <emily.e.serven@gmail.com>
2025-12-07 19:51:03 -0500

    Updated courses index page to use header

--- packages/client/src/routes/courses.index.tsx
+++ packages/client/src/routes/courses.index.tsx
@@ -6,6 +6,7 @@ import { ArrowRightIcon } from "lucide-react";
 
 import { Button } from "@/components/button";
 import { CourseBox } from "@/components/CourseBox";
+import { PageHeader } from "@/components/PageHeader";
 import { fetchCourses } from "@/utils/fetchFunctions";
 
 export const Route = createFileRoute("/courses/")({
@@ -49,45 +50,54 @@ function Courses() {
   });
 
   return (
-    <div className="p-4">
-      <h1 className="mb-4 text-3xl">{local.name}&#39;s Courses</h1>
-      <div
-        className={`
-          grid grid-cols-1 gap-4 gap-y-6
-          sm:grid-cols-2
-          md:grid-cols-3
-        `}
-      >
-        {(!data || data.length === 0) && (
-          <div className="flex flex-col gap-6">
-            <i>No courses yet!</i>
+    <div>
+      <div className="mb-4 bg-gray-200 py-6">
+        <div className="container">
+          <PageHeader
+            pageTitle={`${local.name}'s Courses`}
+            pageSection=""
+          />
+        </div>
+      </div>
+      <div className="container">
+        <div
+          className={`
+            grid grid-cols-1 gap-4 gap-y-6
+            sm:grid-cols-2
+            md:grid-cols-3
+          `}
+        >
+          {(!data || data.length === 0) && (
+            <div className="flex flex-col gap-6">
+              <i>No courses yet!</i>
 
-            <Link
-              to="/onboard"
-              className=""
-            >
-              <Button>
-                Go to onboarding
-                {" "}
-                <ArrowRightIcon />
-              </Button>
-            </Link>
-          </div>
-        )}
+              <Link
+                to="/onboard"
+                className=""
+              >
+                <Button>
+                  Go to onboarding
+                  {" "}
+                  <ArrowRightIcon />
+                </Button>
+              </Link>
+            </div>
+          )}
 
-        {
-          data && data.length > 0 && data.map((course: Course) => {
-            if (!course) {
-              return <></>;
-            }
-            return (
-              <CourseBox
-                {...course}
-                key={course.id}
-              />
-            );
-          })
-        }
+          {
+            data && data.length > 0 && data.map((course: Course) => {
+              if (!course) {
+                return <></>;
+              }
+              return (
+                <CourseBox
+                  {...course}
+                  key={course.id}
+                />
+              );
+            })
+          }
+        </div>
       </div>
     </div>
   );
