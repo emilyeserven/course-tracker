1f207ef639971c754167c20658e8acf99f5b81d1
eserven <emily.e.serven@gmail.com>
2025-12-07 19:49:52 -0500

    Genericized TopicList from TopicTags

--- packages/client/src/components/TopicList.tsx
+++ packages/client/src/components/TopicList.tsx
@@ -2,18 +2,22 @@ import type { Topic } from "@emstack/types/src";
 
 import { Link } from "@tanstack/react-router";
 
-interface TopicTagsProps {
+import { cn } from "@/lib/utils";
+
+interface TopicListProps {
   topics: Topic[] | undefined;
+  isPills?: boolean;
 }
 
-export function TopicTags({
+export function TopicList({
   topics,
-}: TopicTagsProps) {
+  isPills = true,
+}: TopicListProps) {
   if (!topics) {
     return <></>;
   }
   return (
-    <>
+    <div className="flex flex-row items-center gap-2">
       {topics && topics.map((topic) => {
         if (!topic) {
           return <></>;
@@ -25,16 +29,15 @@ export function TopicTags({
             params={{
               id: topic.id + "",
             }}
-            className={`
-              rounded bg-gray-50 px-2 py-0.5 text-xs
-              hover:bg-gray-900 hover:text-white
-            `}
+            className={cn({
+              "rounded bg-gray-50 px-2 py-0.5 text-xs hover:bg-gray-900 hover:text-white": isPills,
+            })}
             key={topic.id}
           >
             {topic.name}
           </Link>
         );
       })}
-    </>
+    </div>
   );
 }
