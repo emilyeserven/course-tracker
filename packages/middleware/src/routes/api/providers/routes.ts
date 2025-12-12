20709b5a2b711731f2e3907988b0c8f3bc59247b
eserven <emily.e.serven@gmail.com>
2025-12-11 21:04:28 -0500

    Added deleteProviders and deleteTopic endpoints

--- packages/middleware/src/routes/api/providers/routes.ts
+++ packages/middleware/src/routes/api/providers/routes.ts
@@ -3,10 +3,12 @@ import { FastifyInstance } from "fastify";
 
 import courseRoot from "./root";
 import getProviders from "./getProviders";
+import deleteProviders from "./deleteProviders";
 
 export default async function (server: FastifyInstance) {
   const fastify = server.withTypeProvider<JsonSchemaToTsProvider>();
 
   fastify.register(courseRoot);
   fastify.register(getProviders);
+  fastify.register(deleteProviders);
 }
